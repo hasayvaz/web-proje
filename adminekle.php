6bd794698898c820010b8a73958f5561133ba1eb
hasan ayvaz <hasayvaz@bil.omu.edu.tr>
2011-06-23 21:01:33 +0300

    admin ekle formu

---
+++ adminekle.php
@@ -0,0 +1,16 @@
+<html>
+<head>  ADMIN KAYDI  </head>
+<body bgcolor = green>
+
+ <form name = "form1" action = "addadmin.php" method = "POST">
+  <pre>
+
+  <b>ad			:</b>
+	<input type = "text" name = "ad" value = ""><br>
+  <b>parola				:</b>
+	<input type = "password" name = "parola" value = "" ><br>	
+  <input type = "submit" value = "Sql'e Ekle"><br>
+  </pre>
+ </form>
+</body>
+</html>
