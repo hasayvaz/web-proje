413207bb5d297f34bf527ca1e9ad074163604f0c
hasan ayvaz <hasayvaz@bil.omu.edu.tr>
2011-06-23 21:01:00 +0300

    admin login

---
+++ adminlogin.php
@@ -0,0 +1,30 @@
+<html>
+<body text="#000000" BGCOLOR="#E3CC9B" target="sag">
+<CENTER>
+<P>
+<h1>ADMIN GIRISI</h1>
+
+</P>
+
+<FORM	action="admin.php" name="girisformu" method="POST">
+   <TABLE BORDER=0 BGCOLOR="#f4a460" cellpadding=0 cellspacing=0>
+     <TR><TD ALIGN=CENTER colspan=5>&nbsp;</TD></TR>
+     <TR>
+        <TD ALIGN=CENTER colspan=5><font color="#890900"><B>numara ve þifre girip Gönder'e týklayýn.</B></font><br></TD>
+     </TR>
+     <TR>
+        <TD ALIGN=LEFT><B><font size=4>&nbsp;&nbsp;ad:&nbsp;</font></B></TD>
+        <TD ALIGN=LEFT><INPUT type=text  size=17 maxlength=15 name = "ad" value = ""></TD>
+        <TD ALIGN=LEFT>&nbsp;&nbsp;<B><font size=4>þifre:&nbsp;</font></B></TD>
+        <TD ALIGN=LEFT><INPUT type=password size=11 maxlength=11 name = "parola" value = ""></TD>
+        <TD ALIGN=LEFT>&nbsp;&nbsp;<INPUT type=submit value="Gönder">&nbsp;&nbsp;</TD>
+     </TR>
+     <TR><TD ALIGN=CENTER colspan=5>&nbsp;</TD></TR>
+   </TABLE>
+</FORM>
+
+
+</CENTER>
+</BODY>
+</HTML>
+ 
