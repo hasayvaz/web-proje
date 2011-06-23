8e92825b2c55032b874d96460f8a8b8107fde273
hasan ayvaz <hasayvaz@bil.omu.edu.tr>
2011-06-23 21:05:24 +0300

    silformu

---
+++ silformu.php
@@ -0,0 +1,16 @@
+<html>
+<head>  =>Girilen bilgilere sahip tüm veriler silinecektir<=  </head>
+<body bgcolor = orange>
+ <form name = "form1" action = "sil.php" method = "POST">
+  <pre>
+
+  numara		:<input type = "text" name = "numara" value = ""><br>
+  sifre         	:<input type = "password" name = "sifresi" value = "" ><br> 	
+
+  <input type = "submit" value = "Veri Sil"><br>
+
+  </pre>
+ </form>
+
+</body>
+</html>
