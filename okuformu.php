a5339c5ef3d6705888ceac57d017cacbaeee62ec
hasan ayvaz <hasayvaz@bil.omu.edu.tr>
2011-06-23 21:04:42 +0300

    okuformu

---
+++ okuformu.php
@@ -0,0 +1,29 @@
+<html>
+<body text="#000000" bGCOLOR="#E3CC9B" target="sag">
+
+<center>
+<p>
+<h1>Ögrenci Girisi</h1>
+
+</p>
+
+<form	action="oku.php" name="girisformu" method="POST">
+   <table BORDER=0 BGCOLOR="#f4a460" cellpadding=0 cellspacing=0>
+     <tr><td ALIGN=CENTER colspan=5>&nbsp;</td></tr>
+     <tr>
+        <td ALIGN=CENTER colspan=5><font color="#890900"><b>numara ve þifre girip Gönder'e týklayýn.</b></font><br></td>
+     </tr>
+     <tr>
+        <td ALIGN=LEFT><b><font size=4>&nbsp;&nbsp;numara:&nbsp;</font></b></td>
+        <td ALIGN=LEFT><INPUT type=text  size=17 maxlength=15 name = "numara" value = ""></td>
+        <td ALIGN=LEFT>&nbsp;&nbsp;<b><font size=4>þifre:&nbsp;</font></b></td>
+        <td ALIGN=LEFT><INPUT type=password size=11 maxlength=11 name = "sifre" value = ""></td>
+        <td ALIGN=LEFT>&nbsp;&nbsp;<INPUT type=submit value="Gönder">&nbsp;&nbsp;</td>
+     </tr>
+     <tr><td ALIGN=CENTER colspan=5>&nbsp;</td></tr>
+   </table>
+</form>
+</center>
+</body>
+</html>
+ 
