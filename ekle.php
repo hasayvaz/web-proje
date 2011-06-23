6a7456bb8891efdd1072a2c22f671f51bb00ac16
hasan ayvaz <hasayvaz@bil.omu.edu.tr>
2011-06-23 21:03:52 +0300

    ekle

---
+++ ekle.php
@@ -0,0 +1,34 @@
+<html>
+<head>  Kayýt  </head>
+<body bgcolor = green>
+
+ <form name = "form1" action = "bagla.php" method = "POST">
+  <pre>
+
+  <b>numara			:</b>
+	<input type = "text" name = "numarasi" value = ""><br>
+  <b>sifre				:</b>
+	<input type = "password" name = "sifresi" value = "" ><br>	
+  <b>sinifi			:</b>
+	<input type = "text" name = "sinifi" value = "" ><br>
+  <b>programlama-2               	:</b>
+	<input type = "text" name = "prog" value = "" >
+	--</b>
+	<input type = "text" name = "prog2" value = "" ><br> 	
+  <b>veri yapýlarý               	:</b>
+	<input type = "text" name = "veriyap" value = "" >
+	--</b>
+	<input type = "text" name = "veriyap2" value = "" ><br> 
+  <b>sayýsal tasarým             	:</b>
+	<input type = "text" name = "saytas" value = "" >
+	--</b>
+	<input type = "text" name = "saytas2" value = "" ><br>
+  <b>mesleki ingilizce           	:</b>
+	<input type = "text" name = "mesing" value = "" >
+	--</b>
+	<input type = "text" name = "mesing2" value = "" ><br>
+  <input type = "submit" value = "Sql'e Ekle"><br>
+  </pre>
+ </form>
+</body>
+</html>
